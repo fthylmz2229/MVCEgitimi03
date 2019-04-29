caea2a413c5c421037f4c420d68af83973741ccc
fatih yilmaz <fthylmz2229@hotmail.com>
2019-04-29 21:36:23 +0300

    Add project files.

---
+++ MVCEgitimi03/Controllers/MVCEgitimi03Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitimi03.Controllers
+{
+    public class MVCEgitimi03Controller : Controller
+    {
+        // GET: MVCEgitimi03
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCEgitimi03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCEgitimi03.MvcApplication" Language="C#" %>
